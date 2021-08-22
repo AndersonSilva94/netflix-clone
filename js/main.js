274d03a54039642c083eea5068a2ff06d05cff72
andy silva <andssilva.23@gmail.com>
2021-08-22 07:33:27 -0400

    feat: :rocket: add a new section of popular movies

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $('.owl-carousel').owlCarousel({
-loop:true,
-margin:10,
-nav:false,
+loop: true,
+margin: 10,
+nav: true,
 responsive:{
     0:{
         items:1
@@ -10,7 +10,43 @@ responsive:{
         items:3
     },
     1000:{
-        items:7
+        items:6
     }
 }
-})
+});
+
+$('.section-indicacoes').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+    {
+    breakpoint: 1024,
+    settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+    }
+    },
+    {
+    breakpoint: 600,
+    settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+    }
+    },
+    {
+    breakpoint: 480,
+    settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+    }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+]
+});
